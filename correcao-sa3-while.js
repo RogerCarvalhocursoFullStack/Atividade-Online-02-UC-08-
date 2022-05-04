84421cce0df98e59f5f1b479da60200b03431e47
RogerCarvalhocursoFullStack <104098555+RogerCarvalhocursoFullStack@users.noreply.github.com>
2022-05-04 20:47:55 -0300

    Add files via upload

---
+++ correcao-sa3-while.js
@@ -0,0 +1,17 @@
+let alunos = 0;
+
+while (alunos <= 17) {
+
+    if(alunos == 0) {
+        console.log("Zero " + alunos);
+    } else {
+        if(alunos % 2 == 0) {
+            console.log("Par " + alunos);
+        } else {
+            console.log("Impar " + alunos);
+        }
+
+
+    contador ++;
+}
+}
